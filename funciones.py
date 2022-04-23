e3f21becd5fd9bf9a7278c5fb63d89baa8e27ffd
andmansim <91722324+andmansim@users.noreply.github.com>
2022-04-23 12:25:48 +0200

    cojo columnas necesarias para el trabajo

--- funciones.py
+++ funciones.py
@@ -2,3 +2,5 @@ import pandas as pd
 
 df = pd.read_csv('StudentsPerformance.csv', delimiter= ',', encoding= 'UTF-8' )
 print(df)
+df_new = pd.DataFrame({'math score': df['math score'], 'reading score': df['reading score'], 'writing score': df['writing score']})
+print(df_new)
