822783f96c3b0e0d7964971276a35698d7ebed11
andmansim <91722324+andmansim@users.noreply.github.com>
2022-04-23 12:53:44 +0200

    comentario

--- funciones.py
+++ funciones.py
@@ -1,6 +1,8 @@
 import pandas as pd
+import numpy as np
 
 df = pd.read_csv('StudentsPerformance.csv', delimiter= ',', encoding= 'UTF-8' )
 print(df)
+#Separar datos importantes
 df_new = pd.DataFrame({'math score': df['math score'], 'reading score': df['reading score'], 'writing score': df['writing score']})
 print(df_new)
