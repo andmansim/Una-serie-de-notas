186a12fcbeea82bd407d0adc48b3060616db07b1
andmansim <91722324+andmansim@users.noreply.github.com>
2022-04-25 21:48:20 +0200

    calculo media

--- funciones.py
+++ funciones.py
@@ -8,3 +8,15 @@ class JMPEstadisticas:
 
     def __init__(self,caracteristica):
         self.caracteristica = caracteristica #columna notas
+
+    def calculoMediaAritmetica(self):
+    
+        n = self.caracteristica.count()
+        print(n)
+        sumaValoresObservaciones = 0
+        mediaAritmetica = 0
+        for valorObservacion in self.caracteristica:
+            sumaValoresObservaciones = sumaValoresObservaciones + valorObservacion
+
+        mediaAritmetica = sumaValoresObservaciones / n
+        return mediaAritmetica 
